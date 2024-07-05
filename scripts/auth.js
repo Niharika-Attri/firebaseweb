af0e490d5ac09f111b785574abb4c602af70e9a9
Niharika_Attri <niharikaattri2005@gmail.com>
2024-07-05 16:25:09 +0530

    signout

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,3 @@
-
 // signup
 
 const signupForm = document.querySelector('#signup-form');
@@ -10,9 +9,19 @@ signupForm.addEventListener('submit', (e) => {
     console.log(email, password);
 
     auth.createUserWithEmailAndPassword(email, password).then(cred =>{
-        console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset()
+    })
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(()=> {
+        console.log("user signed out");
+    }).catch((err) => {
+        console.log("an error occured: ", err);
     })
 })
